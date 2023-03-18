33f17852a1f00add1dcc71642729ffafa12508e2
kait <kingkittykait@gmail.com>
2023-03-17 20:49:54 -0700

    set up tailwind config, skeleton files setup

---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '/style.css';
+import 
+
+function Home() {
+    return (
+        <div className='homepagebackground'>
+        <container>
+        <row>
+        <col-m
+        </row>
+        </container>
+        </div>
+    )
+    
+}
