fa0f706a3f454bcff7c23b529857e6a1db67862c
kait <kingkittykait@gmail.com>
2023-03-19 15:27:52 -0700

    commiting what i have so far

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -1,12 +1,10 @@
 export default function About () {
     return (
-        <div class="bg-red-200 p-6 md:container md:mx-auto">
-        <div className="flex flex-col w-full mt-8">
+        <div class="container shadow-2xl bg-red-200 p-6 md:container md:mx-auto">
             <h1 className="text-xl text-gray-400">Hello! I'm</h1>
             <h1 className="text-2xl font-bold">Kaitlyn Heinle</h1>
             <p class="text-xl font-bold text-gray-300">A Coder</p>
             <p className="text-md font-light text-gray-400 ">Interested in furthering my education in cyber security, game development and back end work.</p>
-        </div>
         </div>
     )
 }
--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
+
 
 export default function Navbar() {
     return (
